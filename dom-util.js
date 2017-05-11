339ed3f6e23ab5bd1b0bcea78fdd2303714b150e
Amos Chen <amoschen614@gmail.com>
2017-05-10 22:31:06 -0700

    implement visitAllNodes recursively to traverse all nodes of a broccoli object

--- dom-util.js
+++ dom-util.js
@@ -1,7 +1,10 @@
 
 const visitAllNodes = function(node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
-  // Your code here
+  callback(node);
+  for (let i = 0; i < node.childNodes.length; i++) {
+    visitAllNodes(node.childNodes[i], callback);
+  }
 };
 
 const flattenTreeToArray = function(node) {
